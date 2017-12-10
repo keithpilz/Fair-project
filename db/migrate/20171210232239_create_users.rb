896b63e7e7cca51b5cc8d2db22acd7fc7d2f0f62
keithpilz <kpilson@gmail.com>
2017-12-10 15:48:35 -0800

    Update readme

---
+++ db/migrate/20171210232239_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false
+      t.boolean :is_subscribed, default: true
+      t.boolean :is_admin, default: false
+
+      t.timestamps
+    end
+  end
+end
