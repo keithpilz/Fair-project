dba4cc1b46261b5bc8fb9218fcd08eda6b32c75b
keithpilz <kpilson@gmail.com>
2017-12-14 23:16:24 -0800

    Update gemfile. Create table with links in emails index

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,6 +1,7 @@
 class Email < ApplicationRecord
   has_many :email_statuses
   has_many :users, through: :email_statuses
+  validates :subject, :body, presence: true
   scope :latest_published_email_subject, -> { where(published: true).last.subject }
   scope :latest_published_email_body, -> { where(published: true).last.body }
 
