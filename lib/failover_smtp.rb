8a3ce21e21cf5e193f2ae9e41848a787950509a2
keithpilz <kpilson@gmail.com>
2017-12-14 20:15:26 -0800

    Add reader method to mail class so that it inherits the config

--- lib/failover_smtp.rb
+++ lib/failover_smtp.rb
@@ -10,9 +10,8 @@ module FailoverSMTP
   end
 
   class Mail < Mail::SMTP
-
     def deliver!(message)
-      FailoverSMTP.smtp_settings_array.each_with_index do |smtp_settings, index|
+      smtp_settings_array.each_with_index do |smtp_settings, index|
         self.settings = settings.merge(smtp_settings)
 
         begin
@@ -20,10 +19,14 @@ module FailoverSMTP
           break
         rescue => e
           puts e
-          next unless (FailoverSMTP.smtp_settings_array.size - 1) == index
+          next unless (smtp_settings_array.size - 1) == index
           raise e
         end
       end
+    end
+
+    def smtp_settings_array
+      @smtp_settings_array || FailoverSMTP.smtp_settings_array
     end
   end
 end
