34fda7f16b681af26bbf19167258daba8668ec28
keithpilz <kpilson@gmail.com>
2017-12-15 11:12:51 -0800

    Clean email controller code and wire backend to subscription email mailer with a button

--- app/models/email.rb
+++ app/models/email.rb
@@ -2,7 +2,6 @@ class Email < ApplicationRecord
   has_many :email_statuses
   has_many :users, through: :email_statuses
   validates :subject, :body, presence: true
-  scope :latest_published_email_subject, -> { where(published: true).last.subject }
-  scope :latest_published_email_body, -> { where(published: true).last.body }
+  scope :latest_published_email, -> { where(is_published: true).last }
 
 end
